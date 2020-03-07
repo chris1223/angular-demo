2d20e2af05dd1631e0122131fe4a6b87149c95bb
chris1223 <chris711223@gmail.com>
2020-03-07 13:33:40 +0800

    套用版型

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { Page1Component } from './page1/page1.component';
-import { Page2Component } from './page2/page2.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { TablesComponent } from './tables/tables.component';
+import { ChartsComponent } from './charts/charts.component';
+import { ColorsComponent } from './utilites/colors/colors.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'page1', component: Page1Component },
-  { path: 'page2', component: Page2Component },
+  { path: 'tables', component: TablesComponent },
+  { path: 'charts', component: ChartsComponent },
+  {
+    path: 'utilites',
+    children: [
+      { path: 'colors', component: ColorsComponent }
+    ]
+  },
   { path: '**', component: NotFoundComponent }
 ];
 
